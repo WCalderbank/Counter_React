344526c005caef8c73bc24b543cf2f8263af1ef1
WCalderbank <wcalderbank99@gmail.com>
2022-02-07 19:54:28 +0000

    Styled and Icons inserted

--- counter_react/src/components/Counter.js
+++ counter_react/src/components/Counter.js
@@ -7,14 +7,22 @@ function Counter() {
     <div className="count-container">
       <h2 className="count"> {count} </h2>
       <div className="button-list">
-        <button className="minus" onClick={() => setCount(count - 1)}>
-          -
+        <button
+          className="minus"
+          onClick={() => setCount(count - 1)}
+          disabled={count === 0}
+        >
+          <i class="far fa-frown"></i>
         </button>
         <button className="plus" onClick={() => setCount(count + 1)}>
-          +
+          <i class="far fa-smile-beam"></i>
         </button>
-        <button className="reset" onClick={() => setCount(0)}>
-          Reset
+        <button
+          className="reset"
+          onClick={() => setCount(0)}
+          disabled={count === 0}
+        >
+          <i class="fas fa-undo"></i>
         </button>
       </div>
     </div>
